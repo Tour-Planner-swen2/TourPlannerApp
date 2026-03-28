0e1a48245c73dbf1c8574a0868f14a87dff12c7f
LeopoldKainz <98959508+leopoldKainz@users.noreply.github.com>
2026-03-28 12:06:21 +0100

    Edited layout in  Dashboard for good visualization of where each part is implemented

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 import { TourList } from './components/tour-list/tour-list';
+import { SearchBar } from './components/search-bar/search-bar';
+import { Map } from './components/map/map';
+import { TourDetails } from './components/tour-details/tour-details';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ TourList],
+  imports: [TourList, SearchBar, Map, TourDetails],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
