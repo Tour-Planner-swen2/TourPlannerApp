4f2cb1523e41632f7ef2670128a6c95aee19b801
if23b187 <if23b187@technikum-wien.at>
2026-04-01 14:22:43 +0200

    Ordnerstruktur angelegt, AuthApiService mit Mock-Usern sowie Login und Register Methoden erstellt

---
+++ src/app/core/api/auth-api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthApiService {
+  private mockUsers: User[] = [
+    {
+      userId: 'user-001',
+      username: 'leopold',
+      password: 'leopold123',
+    },
+    {
+      userId: 'user-002',
+      username: 'luka',
+      password: 'luka123',
+    },
+      {
+        userId: 'user-003', 
+        username: 'ana',
+        password: 'ana123',
+      },
+    ];
+
+    login(username: string, password: string): Observable<User | null> {
+        const user = this.mockUsers.find(
+            u => u.username === username && u.password === password
+        );
+        return of(user ?? null);
+    }
+    
+    register(username: string, password: string): Observable<User> {
+        const newUser: User = {
+            userId: Math.random().toString(36).substring(2),
+            username: username,
+            password: password,
+        };
+        this.mockUsers.push(newUser);
+        return of(newUser);
+        }
+  }
