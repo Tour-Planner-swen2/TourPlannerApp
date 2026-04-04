e03c8dc4347b9bc56348cea0acc2ac197ceab35e
if23b187 <if23b187@technikum-wien.at>
2026-04-04 21:29:19 +0200

    Alerts hinzugefügt beim Login und Register

--- src/app/core/api/auth-api.service.ts
+++ src/app/core/api/auth-api.service.ts
@@ -28,12 +28,26 @@ export class AuthApiService {
         const user = this.mockUsers.find(
             u => u.username === username && u.password === password
         );
+
+        if (!user) {
+          alert('Login failed: Invalid username or password');
+        }
+        
         return of(user ?? null);
     }
     
     register(username: string, password: string): Observable<User | null> {
         const exists = this.mockUsers.some(u => u.username === username);
-        if (exists) return of(null);
+        if (exists) {
+            alert('Username already exists.');
+            return of(null);
+        }
+
+        if (username.length < 3 || password.length < 4) {
+            alert('Username must be at least 3 characters and password at least 4 characters long.');
+            return of(null);
+        }
+
 
         const newUser: User = {
             userId: Math.random().toString(36).substring(2),
